9394405442c1513ab69f7e251478838d6662db3f
sonina-marina <mar1na.sonina@yandex.ru>
2026-05-31 21:26:36 +0300

    feat(pages): implement shop page with CRUD operations

---
+++ src/components/shop/SaveShopModal.tsx
@@ -0,0 +1,121 @@
+import { useEffect, useState } from "react"
+import { Form, Input, Modal } from "antd"
+import InputMask from "react-input-mask"
+import { api } from "../../api/api"
+import type { Shop } from "../../types/shop"
+
+
+type SaveShopModalProps = { 
+    open: boolean
+    shop: Shop | null
+    onClose: () => void
+    onSuccess: () => void
+}
+
+export default function SaveShopModal({
+    open,
+    shop,
+    onClose,
+    onSuccess
+}: SaveShopModalProps) {
+    
+    const [form] = Form.useForm()
+    const isEdit = Boolean(shop?.id)
+
+    useEffect(() => {
+        if (!open) return
+        if (shop) {
+            form.setFieldsValue(shop) 
+        } else {
+            form.resetFields()
+        }
+    }, [shop, open]) 
+
+
+    async function handleSubmit(values: any) {
+        const request = {
+            name: values.name,
+            address: values.address,
+            contact_face: values.contactFace,
+            phone_number: values.phoneNumber,
+            email: values.email
+        }
+
+        if (isEdit && shop) {
+            await api.put(`/shops/`, {
+                id: shop.id,
+                ...request
+            })
+        } else {
+            await api.post("/shops/", request)
+        }
+        form.resetFields()
+        await onSuccess()
+        onClose()
+    }
+
+   return (
+      <Modal
+        open={open}
+        onCancel={onClose}
+        onOk={() => form.submit()}
+        title={isEdit ? "Update Shop" : "Create Shop"}
+        okText={isEdit ? "Update" : "Create"}
+      >
+        <Form
+          form={form}
+          layout="vertical"
+          onFinish={handleSubmit}
+        >
+          <Form.Item
+            label="Name"
+            name="name"
+            rules={[{ required: true, message: "Name is required" }]}
+          >
+            <Input />
+          </Form.Item>
+
+          <Form.Item
+            label="Address"
+            name="address"
+            rules={[{ required: true, message: "Address is required" }]}
+          >
+            <Input />
+          </Form.Item>
+
+          <Form.Item
+            label="Contact Face"
+            name="contactFace"
+            rules={[{ required: true, message: "Contact person is required" }]}
+          >
+            <Input />
+          </Form.Item>
+
+          <Form.Item
+            label="Phone Number"
+            name="phoneNumber"
+            rules={[
+              { required: true, message: "Phone is required" },
+              {
+                pattern: /^\+?[0-9\s\-()]{7,20}$/,
+                message: "Invalid phone number"
+              }
+            ]}
+          >
+            <Input />
+          </Form.Item>
+
+          <Form.Item
+            label="Email"
+            name="email"
+            rules={[
+              { required: true, message: "Email is required" },
+              { type: "email", message: "Invalid email format" }
+            ]}
+          >
+            <Input />
+          </Form.Item>
+        </Form>
+      </Modal>
+    ) 
+}
