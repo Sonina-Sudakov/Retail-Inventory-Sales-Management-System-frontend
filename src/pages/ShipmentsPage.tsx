b897213c7e878ee3061764da945b5f8ab9a6fbe5
sonina-marina <mar1na.sonina@yandex.ru>
2026-05-31 08:25:47 +0300

    feat(pages): create default pages for main entities

---
+++ src/pages/ShipmentsPage.tsx
@@ -0,0 +1,8 @@
+export default function ShipmentsPage() {
+
+    return (
+        <h1 className="text-3xl font-bold">
+            Shipments
+        </h1>
+    )
+}
---
+++ src/pages/ShopsPage.tsx
@@ -0,0 +1,8 @@
+export default function ShopsPage() {
+
+    return (
+        <h1 className="text-3xl font-bold">
+            Shops
+        </h1>
+    )
+}
