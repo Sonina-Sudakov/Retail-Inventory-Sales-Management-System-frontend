246f08e8bb7e25c76de59725a498dc6eb59261a0
sonina-marina <mar1na.sonina@yandex.ru>
2026-06-01 21:02:26 +0300

    feat(pages): implement main page for orders

--- src/pages/OrdersPage.tsx
+++ src/pages/OrdersPage.tsx
@@ -1,8 +1,159 @@
-export default function OrdersPage() {
+import { useEffect, useState } from "react"
+import { useNavigate } from "react-router-dom"
 
-    return (
-        <h1 className="text-3xl font-bold">
-            Orders
-        </h1>
-    )
+import { api } from "../api/api"
+import type { OrderShort } from "../types/orderShort"
+
+import {
+    Table,
+    Input,
+    Button,
+    Card,
+    Typography
+} from "antd"
+
+
+export default function OrderPage() {
+
+    const [orders, setOrders] = useState<OrderShort[]>([])
+
+    const [search, setSearch] = useState("")
+
+    const navigate = useNavigate()
+    
+    async function loadOrders() {
+
+        const response = await api.get("/orders/all")
+
+        const orders: OrderShort[] = response.data.items.map(order => ({
+            id: order.id,
+            shopName: order.to_shop.name,
+            createdBy: order.created_by.fullname,
+            status: order.status,
+            createdAt: new Date(order.created_at).toLocaleString(),
+            acceptedAt: order.accepted_at 
+                ? new Date(order.accepted_at).toLocaleString() : "-" 
+        }))
+
+        setOrders(orders)
+    }
+
+    useEffect(() => {
+        loadOrders()
+    }, [])
+
+
+    const columns = [
+        {
+            title: "ID",
+            dataIndex: "id",
+            key: "id"
+        },
+        {
+            title: "Shop Name",
+            dataIndex: "shopName",
+            key: "shopName"
+        },
+        {
+            title: "Created By User",
+            dataIndex: "createdBy",
+            key: "createdBy"
+        },
+        {
+            title: "Status",
+            dataIndex: "status",
+            key: "status"
+        },
+        {
+            title: "Created At",
+            dataIndex: "createdAt",
+            key: "createdAt"
+        },
+        {
+            title: "Accepted At",
+            dataIndex: "acceptedAt",
+            key: "acceptedAt"
+        },
+
+        {
+            title: "Actions",
+            key: "actions",
+            render: (_: unknown, order: OrderShort) => (
+               <>
+                <Button
+                    onClick={() => {
+                        navigate(`/orders/${order.id}`)
+                    }}
+                    style={{ marginRight: 8 }}
+                >
+                    Details
+                </Button>
+              </>            
+            )
+        }
+    ]
+
+   const filteredOrders = orders.filter(order =>
+        Object.values(order).some(value =>
+            String(value).toLowerCase().includes(search.toLowerCase())
+        )
+    ) 
+
+   return (
+        <div className="p-8">
+
+            <Typography.Title
+                level={1}
+                style={{
+                    textAlign: "left",
+                    marginBottom: 16
+                }}
+            >
+                Orders
+            </Typography.Title>
+
+            <Card>
+
+               <div
+                    style={{
+                        display: "flex",
+                        justifyContent: "space-between",
+                        alignItems: "center",
+                        marginBottom: 16
+                    }}
+                >
+                    <Input
+                        placeholder="Search orders"
+                        value={search}
+                        style={{ width: 500 }}
+                        onChange={(e) => setSearch(e.target.value)}
+                    />
+                    <Button
+                        type="primary"
+                        onClick={() => {
+                           navigate(`/orders/create`) 
+                        }}
+                    >
+                        Create Order
+                    </Button>
+
+                </div> 
+                
+                <Table
+                    rowKey="id"
+                    dataSource={filteredOrders}
+                    columns={columns}
+                    pagination={{
+                        pageSize: 10
+                    }}
+                />
+
+            </Card>
+
+        </div>
+
+    ) 
+
 }
+
+
