11c0286ee34967f0d550f0bb305e11e52f700363
sonina-marina <mar1na.sonina@yandex.ru>
2026-06-01 10:48:15 +0300

    feat(pages): implement product page with CRUD operations

--- src/pages/ProductsPage.tsx
+++ src/pages/ProductsPage.tsx
@@ -1,8 +1,183 @@
-export default function ProductsPage() {
 
-    return (
-        <h1 className="text-3xl font-bold">
-            Products
-        </h1>
-    )
+import { useEffect, useState } from "react"
+
+import SaveProductModal from "../components/product/SaveProductModal"
+import { api } from "../api/api"
+import type { Product } from "../types/product"
+
+import {
+    Table,
+    Input,
+    Select,
+    Button,
+    Card,
+    Space,
+    Typography,
+    Popconfirm
+} from "antd"
+
+
+export default function ProductPage() {
+
+    const [products, setProducts] = useState<Product[]>([])
+
+    const [search, setSearch] = useState("")
+
+    const [saveOpen, setSaveOpen] = useState(false)
+    const [selectedProduct, setSelectedProduct] = useState<Product | null>(null)
+
+    async function loadProducts() {
+
+        const response = await api.get("/products/all")
+
+        setProducts(response.data.items)
+    }
+
+    useEffect(() => {
+        loadProducts()
+    }, [])
+
+
+    async function deleteProduct(id: number) {
+
+        await api.delete(`/products?id=${id}`)
+
+        await loadProducts()
+    }
+
+
+    const columns = [
+        {
+            title: "ID",
+            dataIndex: "id",
+            key: "id"
+        },
+        {
+            title: "Name",
+            dataIndex: "name",
+            key: "name"
+        },
+        {
+            title: "Unit",
+            dataIndex: "unit",
+            key: "unit"
+        },
+        {
+            title: "Type",
+            dataIndex: "type",
+            key: "type"
+        },
+        {
+            title: "Price",
+            dataIndex: "price",
+            key: "price"
+        },
+        {
+            title: "Origin",
+            dataIndex: "origin",
+            key: "origin"
+        },
+
+        {
+            title: "Actions",
+            key: "actions",
+            render: (_: unknown, product: Product) => (
+               <>
+                <Button
+                    onClick={() => {
+                        console.log("CREATE CLICK")
+                        setSelectedProduct(product)
+                        setSaveOpen(true)
+                    }}
+                    style={{ marginRight: 8 }}
+                >
+                    Update Info
+                </Button>
+
+                <Popconfirm
+                    title="Delete product?"
+                    onConfirm={() => deleteProduct(product.id)}
+                >
+                    <Button danger>
+                        Delete
+                    </Button>
+                </Popconfirm>
+              </>            
+            )
+        }
+    ]
+
+   const filteredProducts = products.filter(product =>
+        Object.values(product).some(value =>
+            String(value).toLowerCase().includes(search.toLowerCase())
+        )
+    ) 
+
+   return (
+        <div className="p-8">
+
+            <Typography.Title
+                level={1}
+                style={{
+                    textAlign: "left",
+                    marginBottom: 16
+                }}
+            >
+                Products
+            </Typography.Title>
+
+            <Card>
+
+               <div
+                    style={{
+                        display: "flex",
+                        justifyContent: "space-between",
+                        alignItems: "center",
+                        marginBottom: 16
+                    }}
+                >
+                    <Input
+                        placeholder="Search products"
+                        value={search}
+                        style={{ width: 500 }}
+                        onChange={(e) => setSearch(e.target.value)}
+                    />
+
+                    <Button
+                        type="primary"
+                        onClick={() => {
+                            setSelectedProduct(null) 
+                            setSaveOpen(true)
+                        }}
+                    >
+                        Create Product
+                    </Button>
+                </div> 
+                
+                <Table
+                    rowKey="id"
+                    dataSource={filteredProducts}
+                    columns={columns}
+                    pagination={{
+                        pageSize: 10
+                    }}
+                />
+
+            </Card>
+
+            <SaveProductModal
+                open={saveOpen}
+                product={selectedProduct}
+                onClose={() => {
+                    setSaveOpen(false)
+                    setSelectedProduct(null)
+                }}
+                onSuccess={loadProducts}
+            />
+
+            </div>
+
+    ) 
+
 }
+
