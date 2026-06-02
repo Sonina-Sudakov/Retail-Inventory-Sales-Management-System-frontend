d9d5a8f9d07a43e047d1fb865c163d08517557f7
sonina-marina <mar1na.sonina@yandex.ru>
2026-06-02 22:13:47 +0300

    feat(pages): start implement shipments page

--- src/pages/ShipmentsPage.tsx
+++ src/pages/ShipmentsPage.tsx
@@ -1,8 +1,183 @@
-export default function ShipmentsPage() {
+import { useEffect, useState } from "react"
+import { useNavigate } from "react-router-dom"
+
+import { api } from "../api/api"
+import type { ShipmentShort } from "../types/shipmentShort"
+
+import {
+    Table,
+    Input,
+    Button,
+    Card,
+    Typography
+} from "antd"
+import { getRole } from "../utils/jwt"
+
+
+export default function ShipmentPage() {
+
+    const [shipments, setShipments] = useState<ShipmentShort[]>([])
+
+    const [search, setSearch] = useState("")
+
+    const navigate = useNavigate()
+
+    const role = getRole()?.toLowerCase()
+
+    async function loadShipments() {
+
+        const response = await api.get("/shipments/all")
+
+        const shipments: ShipmentShort[] = response.data.items.map(shipment => ({
+            id: shipment.id,
+            fromLocation: shipment.to_shop.name,
+            createdBy: shipment.created_by.fullname,
+            status: shipment.status,
+            createdAt: new Date(shipment.created_at).toLocaleString(),
+            updatedAt: shipment.updated_at
+                ? new Date(shipment.updated_at).toLocaleString() : "-"
+        }))
+
+        setShipments(shipments)
+    }
+
+    useEffect(() => {
+        loadShipments()
+    }, [])
+
+
+    const columns = [
+        {
+            title: "ID",
+            dataIndex: "id",
+            key: "id"
+        },
+        {
+            title: "From Location",
+            dataIndex: "fromLocation",
+            key: "fromLocation"
+        },
+        {
+            title: "Created By User",
+            dataIndex: "createdBy",
+            key: "createdBy"
+        },
+        {
+            title: "Status",
+            dataIndex: "status",
+            key: "status",
+            filters: [
+                {
+                    text: "Canceled",
+                    value: "CANCELED"
+                },
+                {
+                    text: "Updated",
+                    value: "UPDATED"
+                },
+                {
+                    text: "Pending",
+                    value: "PENDING"
+                }
+            ],
+            defaultFilteredValue: ["PENDING"],
+
+            onFilter: (value, record) =>
+
+                record.status === value
+        },
+        {
+            title: "Created At",
+            dataIndex: "createdAt",
+            key: "createdAt"
+        },
+        {
+            title: "Updated At",
+            dataIndex: "updatedAt",
+            key: "updatedAt"
+        },
+
+        {
+            title: "Actions",
+            key: "actions",
+            render: (_: unknown, shipment: ShipmentShort) => (
+                <>
+                    <Button
+                        onClick={() => {
+                            navigate(`${shipment.id}`)
+                        }}
+                        style={{ marginRight: 8 }}
+                    >
+                        Details
+                    </Button>
+                </>
+            )
+        }
+    ]
+
+    const filteredShipments = shipments.filter(shipment =>
+        Object.values(shipment).some(value =>
+            String(value).toLowerCase().includes(search.toLowerCase())
+        )
+    )
 
     return (
-        <h1 className="text-3xl font-bold">
-            Shipments
-        </h1>
+        <div className="p-8">
+
+            <Typography.Title
+                level={1}
+                style={{
+                    textAlign: "left",
+                    marginBottom: 16
+                }}
+            >
+                Shipments
+            </Typography.Title>
+
+            <Card>
+
+                <div
+                    style={{
+                        display: "flex",
+                        justifyContent: "space-between",
+                        alignItems: "center",
+                        marginBottom: 16
+                    }}
+                >
+                    <Input
+                        placeholder="Search shipments"
+                        value={search}
+                        style={{ width: 500 }}
+                        onChange={(e) => setSearch(e.target.value)}
+                    />
+                    {role === "shopkeeper" && (
+                        <Button
+                            type="primary"
+                            onClick={() => {
+                                navigate(`create`)
+                            }}
+                        >
+                            Create Shipment
+                        </Button>
+                    )}
+
+                </div>
+
+                <Table
+                    rowKey="id"
+                    dataSource={filteredShipments}
+                    columns={columns}
+                    pagination={{
+                        pageSize: 10
+                    }}
+                />
+
+            </Card>
+
+        </div>
+
     )
+
 }
+
+
