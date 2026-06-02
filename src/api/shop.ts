2a4e518d9e86f8d0f4d78772ef6519eb09fc8e8f
egogoboy <egor2005sudakov@gmail.com>
2026-06-02 20:45:03 +0300

    feat(api): implement getShop() and getStocks() in shop

--- src/api/shop.ts
+++ src/api/shop.ts
@@ -3,3 +3,11 @@ import { api } from "./api";
 export async function getShops() {
     return api.get("/shops/all");
 }
+
+export async function getShop(id: number) {
+    return api.get(`/shops?id=${id}`);
+}
+
+export async function getStocks(shop_id: number) {
+    return api.get(`shops/${shop_id}/stocks`)
+}
